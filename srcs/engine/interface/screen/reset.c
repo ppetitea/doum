1634952c0386b1a480ea75d8bb2954d2899fdc10
ppetitea <pierre.petiteau.4985@gmail.com>
2020-01-28 23:22:42 +0100

    fix libft and screen reset

--- srcs/engine/interface/screen/reset.c
+++ srcs/engine/interface/screen/reset.c
@@ -6,7 +6,7 @@
 /*   By: ppetitea <ppetitea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 18:24:16 by ppetitea          #+#    #+#             */
-/*   Updated: 2020/01/28 18:20:06 by ppetitea         ###   ########.fr       */
+/*   Updated: 2020/01/28 23:16:02 by ppetitea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,8 @@ void	reset_screen(t_screen screen)
 
 	i = 0;
 	while (i < sizeof(uint32_t) * screen.size.x * screen.size.y)
+	{
 		screen.pixels[i] = 0x00000000;
+		i++;
+	}
 }
